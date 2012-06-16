0d9cda5ce19577c0c682d25571f6981b67e4af87
Simon Hengel <sol@typeful.net>
2012-06-16 12:16:14 +0200

    shouldSatisfyMonoidLaws: Use <>

--- src/Test/Hspec/Laws.hs
+++ src/Test/Hspec/Laws.hs
@@ -1,23 +1,20 @@
 -- | Reusable specifications.
 module Test.Hspec.Laws (shouldSatisfyMonoidLaws) where
 
-import           Prelude hiding ((++))
+import           Prelude
 import           Data.Monoid
 import           Test.QuickCheck
 import           Test.Hspec.ShouldBe
-
-(++) :: Monoid a => a -> a -> a
-(++) = mappend
 
 shouldSatisfyMonoidLaws :: (Eq a, Show a, Monoid a, Arbitrary a) => a -> Specs
 shouldSatisfyMonoidLaws t = do
   describe "mempty" $ do
     it "is a left identity" $ property $ \x ->
-      mempty ++ x == x `asTypeOf` t
+      mempty <> x == x `asTypeOf` t
 
     it "is a right identity" $ property $ \x ->
-      x ++ mempty == x `asTypeOf` t
+      x <> mempty == x `asTypeOf` t
 
   describe "mappend" $ do
     it "is associative" $ property $ \x y z ->
-      (x ++ y) ++ z == x ++ (y ++ z) `asTypeOf` t
+      (x <> y) <> z == x <> (y <> z) `asTypeOf` t
